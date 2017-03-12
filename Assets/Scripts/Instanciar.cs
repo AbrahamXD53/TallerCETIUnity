66af52ea2dbded7feb8dbf9e545114b2b41299b3
Abraham Ahumada <Abraham Ahumada>
2017-03-11 18:52:31 -0600

    Avance del dia 11/03

---
+++ Assets/Scripts/Instanciar.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instanciar : MonoBehaviour {
+
+	public GameObject prefab;
+
+	public float espera=1f;
+
+	void Start () {
+		StartCoroutine(Spawn());
+	}
+
+	IEnumerator Spawn(){
+		Instantiate(prefab,transform.position,Quaternion.identity);
+		yield return new WaitForSeconds(espera);
+		StartCoroutine(Spawn());
+	}
+	
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Movimiento.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimiento : MonoBehaviour {
+
+	public int puntos=0;
+	
+
+	void Awake()
+	{
+		Debug.Log("He despertado!");
+	}
+
+	void Start () {
+		puntos=100;
+		print("Has ganado 100 puntos");
+	}
+	
+	void Update () {
+		puntos++;
+		if(Input.GetKeyDown(KeyCode.Space)){
+			Debug.Log("Quizas podria brincar");
+
+			transform.Translate(new Vector3(0, 2f,0));
+		}
+	}
+}
